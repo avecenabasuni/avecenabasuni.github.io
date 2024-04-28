ca35a072e94e5da34a628f2f6f3c13e291678801
avecenabasuni <71869333+avecenabasuni@users.noreply.github.com>
2024-04-28 18:50:33 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,3 +133,41 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
   });
 }
+
+const startstop = document.getElementById("startstop");
+const seconds = document.getElementById("seconds");
+const minutes = document.getElementById("minutes");
+const hours = document.getElementById("hours");
+const days = document.getElementById("days");
+
+const today = new Date();
+const todayAt12 = new Date();
+todayAt12.setHours(0, 0, 0, 0);
+const birthdate = new Date("01/06/2001");
+let totalSeconds = 0;
+if (todayAt12.getTime() === birthdate.getTime()) {
+  totalSeconds = 0;
+} else {
+  birthdate.setFullYear(today.getFullYear() + 1);
+  totalSeconds = (birthdate - today) / 1000;
+}
+
+const setDisplay = (sec) => {
+  days.innerText = String(Math.floor(sec / (24 * 60 * 60))).padStart(2, "0");
+  sec = sec % (24 * 60 * 60);
+  hours.innerText = String(Math.floor(sec / (60 * 60))).padStart(2, "0");
+  sec = sec % (60 * 60);
+  minutes.innerText = String(Math.floor(sec / 60)).padStart(2, "0");
+  sec = sec % 60;
+  seconds.innerText = String(Math.floor(sec)).padStart(2, "0");
+};
+
+const timer = setInterval(() => {
+  if (totalSeconds <= 0) {
+    totalSeconds = 0;
+    clearInterval(timer);
+    return;
+  }
+  totalSeconds -= 1;
+  setDisplay(totalSeconds);
+}, 1000);
